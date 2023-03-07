b7355edcfef20dd797ac82c8508da439b96e0b02
hakkd <ehakki@alumni.ubc.ca>
2023-03-06 18:16:25 -0800

    fixed code coverage in exception tests

---
+++ src/test/model/NoPeopleExceptionTest.java
@@ -0,0 +1,11 @@
+package model;
+
+import org.junit.jupiter.api.Test;
+
+class NoPeopleExceptionTest extends PersonExceptionTest {
+
+    @Test
+    void testGetMessage() {
+        super.testGetMessage();
+    }
+}
