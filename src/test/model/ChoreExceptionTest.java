63025f7c8033ebba5576852853e632f6c183ba45
hakkd <ehakki@alumni.ubc.ca>
2023-03-06 17:23:34 -0800

    added tests for getMessage() in exceptions

---
+++ src/test/model/ChoreExceptionTest.java
@@ -0,0 +1,15 @@
+package model;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ChoreExceptionTest {
+
+    private ChoreException exception;
+
+    @Test
+    void testGetMessage() {
+        exception = new ChoreException("test message");
+        assertEquals("test message", exception.getMessage());
+    }
+}
